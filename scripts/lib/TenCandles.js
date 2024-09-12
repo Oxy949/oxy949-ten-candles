2fe02c489a8838443a00672de32733596c64cd1c
Konstantin Kustov <oxy949@gmail.com>
2024-09-12 14:41:25 +0300

    init fix

--- scripts/lib/TenCandles.js
+++ scripts/lib/TenCandles.js
@@ -1,7 +1,10 @@
 export class TenCandles {
   
-    static test(operation) 
-    {
-      console.warn(`about-time  Non master timekeeper attempting to ${operation}`)
-    }
+  static init() {
+
+  }
+
+  static test(operation) {
+    console.warn(`about-time  Non master timekeeper attempting to ${operation}`)
+  }
 }  
