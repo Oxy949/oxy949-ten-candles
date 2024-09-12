a96f5788ef70a6236346fbdac380c3ff9e928591
Konstantin Kustov <oxy949@gmail.com>
2024-09-12 15:34:14 +0300

    Fixes

--- scripts/lib/TenCandles.js
+++ scripts/lib/TenCandles.js
@@ -4,24 +4,32 @@ export class TenCandles {
 
   }
 
-  static checkTimer(startedTime, burnFinalTime, checkEverySecTemp) {
-
-    let timeElapsed = (Date.now() - startedTime) / 1000; // Время в секундах
-
-    console.log("Прощло секунд " + timeElapsed);
-    //console.log("Свеча потухла спустя " + timeElapsed.toFixed(2) + " секунд.");
-    game.abouttime.doIn({ second: checkEverySecTemp}, checkTimer, startedTime, burnFinalTime, checkEverySecTemp);
-  }
-
   static startTimer(minBurnTime = 10, maxBurnTime = 30, checkEverySec = 1) {
-
     // Генерируем случайное время в пределах минимального и максимального времени
-    let burnTime = Math.floor(Math.random() * (maxBurnTime - minBurnTime + 1)) + minBurnTime;
+    const burnTime = Math.floor(Math.random() * (maxBurnTime - minBurnTime + 1)) + minBurnTime;
 
     console.log(`Свеча будет гореть в течение ${burnTime} секунд.`);
 
+    // Определяем функцию checkTime
+    function checkTime(startedTime, burnFinalTime, checkEverySecTemp) {
+      let timeElapsed = (Date.now() - startedTime) / 1000; // Время в секундах
 
+      console.log("Прошло секунд " + timeElapsed);
+
+      // Проверяем, потухла ли свеча
+      if (timeElapsed >= burnFinalTime) {
+        console.log("Свеча потухла спустя " + timeElapsed.toFixed(2) + " секунд.");
+        return; // Прекращаем выполнение дальнейших проверок
+      }
+
+      // Повторный вызов функции
+      abouttime.doIn({ second: checkEverySecTemp }, checkTime, startedTime, burnFinalTime, checkEverySecTemp);
+    }
+
+    // Устанавливаем начальное время
     let startTime = Date.now();
-    checkTimer(startTime, burnTime, checkEverySec);
+
+    // Запускаем таймер
+    abouttime.doIn({ second: checkEverySec }, checkTime, startTime, burnTime, checkEverySec);
   }
 }  
